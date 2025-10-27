d6acdfaf6238518bbe700d1025611891ece10e51
Beh LC <behlc2@gmail.com>
2025-10-27 18:59:11 +0800

    Stars UI and more stars

--- Assets/Scripts/Stars.cs
+++ Assets/Scripts/Stars.cs
@@ -1,12 +1,17 @@
 using UnityEngine;
+using TMPro;
 
 public class Stars : MonoBehaviour
 {
     public AudioClip starsClip;
+    private TextMeshProUGUI starsText;
+    public int starsToGive = 1;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // Find the Stars Text game object; then get its component
+        starsText = GameObject.FindWithTag("StarsText").GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
@@ -21,8 +26,9 @@ public class Stars : MonoBehaviour
         {
             // access to the player script
             Player player = collision.gameObject.GetComponent<Player>();
-            player.stars += 1;
+            player.stars += starsToGive;
             player.PlaySFX(starsClip, 0.1f);
+            starsText.text = player.stars.ToString();
 
             Destroy(gameObject);
         }
